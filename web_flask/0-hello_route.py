a4b5468e6dbde9b93119de1de24e5e5079e7881a
72119sisay <sisayfikru69@gmail.com>
2023-09-11 07:00:34 +0000

    web_flask/0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,16 +3,16 @@
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask,render_template
+from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return render_template("10-hbnb_filters.html")
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """ Display 'Hello HBNB! """
+    return "Hello HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host='0.0.0.0', port=5000)
